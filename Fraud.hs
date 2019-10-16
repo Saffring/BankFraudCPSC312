1fb1a9c96290fe99cff21b10e09e38da0f79898a
Saffrin Granby <35471618+Saffring@users.noreply.github.com>
2019-10-15 22:10:45 -0700

    Only take into account purchase transactions

--- Fraud.hs
+++ Fraud.hs
@@ -10,9 +10,9 @@ import Data.List
 
 getpurchasesaverage :: [Transaction] -> Double
 getpurchasesaverage [] = 0
-getpurchasesaverage transactions = (sumpurchases transactions)/(genericLength transactions)
+getpurchasesaverage transactions = (sumpurchases transactions)/(genericLength (getpurchasetransactions transactions))
 
 getdailytotalaverage :: [Transaction] -> Double
 getdailytotalaverage [] = 0
 getdailytotalaverage transactions = (sum dailylist)/(genericLength dailylist)
-    where dailylist = createdailysumlist transactions
+    where dailylist = createdailysumlist (getpurchasetransactions transactions)
